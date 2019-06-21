71f0a489404e53debae17e86e01a49dc69b0a050
Liu <bill.liu@sap.com>
2019-06-21 17:09:01 -0400

    cert

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -1,4 +1,4 @@
 var path = require('path'),
 fs = require("fs");
-exports.privateKey = fs.readFileSync(path.join(__dirname, './private/key.pem')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, './private/cert.pem')).toString();
+exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privkey1.pem')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, './private/cert1.pem')).toString();
